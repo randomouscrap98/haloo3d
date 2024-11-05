5119287b7295847f2b8b2a9a38cf569883bf31be
Carlos Sanchez <randomouscrap98@aol.com>
2024-11-05 04:08:52 -0500

    Verified seams are model uv not polygons

--- tests/t2_simpleobj.c
+++ tests/t2_simpleobj.c
@@ -2,11 +2,13 @@
 #include "../haloo3dex_obj.h"
 #include "../haloo3dex_unigi.h"
 
+#include <math.h>
+
 // simple afine texture mapped triangle
 void triangle(h3d_rastervertex *rv, h3d_fb *buf, h3d_fb *tex, uint16_t bw,
               uint16_t bh) {
   H3DTRI_EASY_BEGIN(rv, bw, bh, linpol, 3, bufi) {
-    if (linpol[2] > buf->dbuffer[bufi]) {
+    if (linpol[2] < buf->dbuffer[bufi]) {
       buf->dbuffer[bufi] = linpol[2];
       buf->buffer[bufi] = h3d_fb_getuv(tex, linpol[0], linpol[1]);
     }
@@ -29,7 +31,8 @@ int main(int argc, char **argv) {
   h3d_fb_init(&screen, WIDTH, HEIGHT);
   const int len = h3d_fb_size(&screen);
   for (int i = 0; i < len; i++) {
-    screen.dbuffer[i] = H3DVF(0);
+    screen.buffer[i] = 0xF0F0;
+    screen.dbuffer[i] = H3DVF(999999);
   }
   eprintf("Initialized screen fb\n");
 
@@ -47,14 +50,15 @@ int main(int argc, char **argv) {
   // Iterate over all triangles
   for (int i = 0; i < model.numfaces; i++) {
     for (int v = 0; v < 3; v++) {
-      rv[v].pos[H3DX] =
-          WIDTH * (model.vertices[model.faces[i][v].verti][H3DX] + 1) / 2;
-      rv[v].pos[H3DY] =
-          HEIGHT * (1 - model.vertices[model.faces[i][v].verti][H3DY]) / 2;
+      rv[v].pos[H3DX] = round(
+          WIDTH * 0.5 * (model.vertices[model.faces[i][v].verti][H3DX] + 1));
+      rv[v].pos[H3DY] = round(
+          HEIGHT * 0.5 * (1 - model.vertices[model.faces[i][v].verti][H3DY]));
       rv[v].interpolants[0] = model.vtexture[model.faces[i][v].texi][H3DX];
       rv[v].interpolants[1] = model.vtexture[model.faces[i][v].texi][H3DY];
       rv[v].interpolants[2] =
-          1 / (1 - model.vertices[model.faces[i][v].verti][H3DZ]);
+          1 / (3 + model.vertices[model.faces[i][v].verti][H3DZ]);
+      // 3 is just an arbitrary number to make sure z is never 0
     }
     triangle(rv, &screen, &tex, WIDTH, HEIGHT);
   }
